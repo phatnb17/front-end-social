0c6088daa83d0bea86c458205756e890010aab79
J.A.R.V.I.S <phatnb@candylio.com>
2020-11-15 17:49:45 +0700

    #19 Signup and Auth Route

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -9,6 +9,9 @@ import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
+//redux stuff
+import { connect } from "react-redux";
+import { signupUser } from "../redux/actions/userActions";
 
 // const styles = (theme) => ({
 //   ...theme.spreadThis,
@@ -24,9 +27,15 @@ class signup extends Component {
       password: "",
       confirmPassword: "",
       handle: "",
-      loading: false,
       errors: {},
     };
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.UI.errors) {
+      this.setState({
+        errors: nextProps.UI.errors,
+      });
+    }
   }
   handleSubmit = (event) => {
     event.preventDefault();
@@ -39,22 +48,7 @@ class signup extends Component {
       confirmPassword: this.state.confirmPassword,
       handle: this.state.handle,
     };
-    axios
-      .post("/signup", newUserData)
-      .then((res) => {
-        console.log(res.data);
-        localStorage.setItem("FBIdToken", `Bearer ${res.data.token}`);
-        this.setState({
-          loading: false,
-        });
-        this.props.history.push("/");
-      })
-      .catch((err) => {
-        this.setState({
-          errors: err.response.data,
-          loading: false,
-        });
-      });
+    this.props.signupUser(newUserData, this.props.history);
   };
   handleChange = (event) => {
     this.setState({
@@ -62,8 +56,11 @@ class signup extends Component {
     });
   };
   render() {
-    const { classes } = this.props;
-    const { errors, loading } = this.state;
+    const {
+      classes,
+      UI: { loading },
+    } = this.props;
+    const { errors } = this.state;
     return (
       <Grid container className={classes.form}>
         <Grid item sm></Grid>
@@ -157,8 +154,17 @@ class signup extends Component {
     );
   }
 }
+
 signup.propTypes = {
   classes: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+  UI: PropTypes.object.isRequired,
+  signupUser: PropTypes.func.isRequired,
 };
-
-export default withStyles(styles)(signup);
+const mapStateToProps = (state) => ({
+  user: state.user,
+  UI: state.UI,
+});
+export default connect(mapStateToProps, { signupUser })(
+  withStyles(styles)(signup)
+);
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,12 +1,5 @@
-import {
-  SET_USER,
-  SET_ERRORS,
-  CLEAR_ERRORS,
-  LOADING_UI,
-  SET_AUTHENTICATED,
-  SET_UNAUTHENTICATED,
-} from "../types";
-import axios from "axios";
+import { SET_USER, SET_AUTHENTICATED, SET_UNAUTHENTICATED } from "../types";
+//import axios from "axios";
 
 const initialState = {
   authenticated: false,
