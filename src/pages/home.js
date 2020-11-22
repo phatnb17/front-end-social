341000fd9a701f7c24091bc1d5df41955828f547
J.A.R.V.I.S <phatnb@candylio.com>
2020-11-22 18:32:39 +0700

    #20 profile

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import Grid from "@material-ui/core/Grid";
 import Scream from "../components/Scream";
+import Profile from "../components/Profile";
 class home extends Component {
   state = {
     screams: null,
@@ -31,7 +32,7 @@ class home extends Component {
           {recentScreamsMarkup}
         </Grid>
         <Grid item sm={4} xs={12}>
-          <p>profile...</p>
+        <Profile/>
         </Grid>
       </Grid>
     );
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -4,6 +4,7 @@ import {
   CLEAR_ERRORS,
   LOADING_UI,
   SET_UNAUTHENTICATED,
+  LOADING_USER,
 } from "../types";
 import axios from "axios";
 
@@ -30,6 +31,7 @@ export const logoutUser = (dispatch) => {
   dispatch({ type: SET_UNAUTHENTICATED });
 };
 export const getUserData = () => (dispatch) => {
+ dispatch({ type:LOADING_USER});
   axios
     .get("/user")
     .then((res) => {
