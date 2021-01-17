b6765992a87c706037637213b1d20c49710420ea
J.A.R.V.I.S <phatnb@candylio.com>
2021-01-17 12:41:42 +0700

    #25 delete scream

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,4 +1,11 @@
-import { SET_USER, SET_AUTHENTICATED, SET_UNAUTHENTICATED, LOADING_USER} from "../types";
+import {
+  SET_USER,
+  SET_AUTHENTICATED,
+  SET_UNAUTHENTICATED,
+  LOADING_USER,
+  LIKE_SCREAM,
+  UNLIKE_SCREAM
+} from "../types";
 //import axios from "axios";
 
 const initialState = {
@@ -23,12 +30,30 @@ export default function (state = initialState, action) {
         loading: false,
         ...action.payload,
       };
-      case LOADING_USER:
-        return{
-          ...state,
-          loading: true,
-        }
+    case LOADING_USER:
+      return {
+        ...state,
+        loading: true,
+      };
+    case LIKE_SCREAM:
+      return {
+        ...state,
+        likes: [
+          ...state.likes,
+          {
+            userHandle: state.credentials.handle,
+            screamId: action.payload.screamId,
+          },
+        ],
+      };
+    case UNLIKE_SCREAM:
+      return {
+        ...state,
+        likes: state.likes.filter(
+          (like) => like.screamId !== action.payload.screamId
+        ),
+      };
     default:
       return state;
-  } 
+  }
 }
